47dee8ca842bca73120982985a7cf07a6e559545
Palveet Kaur Saluja <52796065+Palveet@users.noreply.github.com>
2024-11-23 23:21:48 -0600

    remove apps

--- apps/resume_builder_django/resume/url.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ResumeView
-
-urlpatterns = [
-    path('resumes/', ResumeView.as_view(), name='resume-list'),
-    path('resumes/<int:pk>/', ResumeView.as_view(), name='resume-detail'),
-]
